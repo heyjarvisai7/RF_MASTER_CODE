b75a4f930288d336cf4b7a0a97c92fb400dab797
Jarvis <heyjarvisai7@gmail.com>
2026-01-30 12:30:13 +0530

    Update main.c

--- fun_mcode/nRF5_SDK_17.1.0_ddde560/examples/proprietary_rf/esb_prx29/main.c
+++ fun_mcode/nRF5_SDK_17.1.0_ddde560/examples/proprietary_rf/esb_prx29/main.c
@@ -97,6 +97,9 @@ static nrf_esb_payload_t        tx_payload = NRF_ESB_CREATE_PAYLOAD(0, 0x01, 0x0
 #define     MAX_NODES           255
 #define     MIN_NODES           0  
 
+#define DAMU 7
+#define Frnd 9
+
 
 #define check_direction(arrptr) \
     (((arrptr)[POS_DIRECTION]  == 1) ? (Fwrd_Dirct = 1) : (Rvsr_Dirct = 1))
